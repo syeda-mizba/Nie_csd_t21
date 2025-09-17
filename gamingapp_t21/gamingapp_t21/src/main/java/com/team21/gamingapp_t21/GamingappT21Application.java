6cef23f10b9dc7b2802aec7b9a2b7d91a133c2ed
syeda-mizba <mizbaaasyedaaa@gmail.com>
2025-09-17 09:07:34 +0530

    commit

---
+++ gamingapp_t21/gamingapp_t21/src/main/java/com/team21/gamingapp_t21/GamingappT21Application.java
@@ -0,0 +1,13 @@
+package com.team21.gamingapp_t21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingappT21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingappT21Application.class, args);
+	}
+
+}
---
+++ gamingapp_t21/gamingapp_t21/src/test/java/com/team21/gamingapp_t21/GamingappT21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team21.gamingapp_t21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingappT21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
